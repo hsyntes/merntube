f735a3489973e00e1c3207bbaa16d1bf42e8e905
hsyntes <se.hsyntes@gmail.com>
2023-09-01 15:22:36 +0300

    update Button component

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -1,14 +1,14 @@
 const Button = ({ type, variant, className, onClick, children }) => {
-  let classes = `font-semibold text-white text-sm rounded shadow py-2 px-4 transition ${className} `;
+  let classes = `font-semibold text-sm rounded shadow py-2 px-4 transition ${className} `;
 
-  if (variant === "primary") classes += "bg-primary hover:bg-primary-darker";
-  if (variant === "dark") classes += "bg-dark hover:bg-dark-darker";
-  if (variant === "light") classes += "bg-light hover:bg-white";
-  if (variant === "white") classes += "bg-white";
+  if (variant === "primary")
+    classes += "bg-primary text-white hover:bg-primary-darker";
+  if (variant === "dark") classes += "bg-dark text-white hover:bg-dark-darker";
+  if (variant === "light") classes += "bg-light text-dark hover:bg-white";
+  if (variant === "white") classes += "bg-white text-dark";
   if (variant === "none")
     classes += "bg-none text-dark dark:text-white !shadow-none";
-  if (variant === "link")
-    classes += "shadow-none !text-dark dark:!text-white !px-0 !py-0";
+  if (variant === "link") classes += "!shadow-none text-primary !px-0 !py-0";
   if (variant === "blue") classes += "bg-blue-700 hover:bg-blue-900 text-white";
   if (variant === "pink") classes += "bg-pink-700 hover:bg-pink-900 text-white";
 
